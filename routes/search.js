874d49d1fecd8be17f89f0297ac01ca02a8f1973
Marko <makaveli13@Markos-MacBook-Pro.local>
2019-09-17 12:09:50 +0200

    made axios working for adress bar

--- routes/search.js
+++ routes/search.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const GreenSpace = require("../models/GreenSpace");
 const User = require("../models/User");
-const axios = require('axios');
-
+const axios = require("axios");
 
 router.get("/", (req, res, next) => {
   res.render("./search/result-map");
@@ -36,11 +35,16 @@ router.get("/location", (req, res, next) => {
 });
 
 router.post("/address", (req, res, next) => {
-  const {address} = req.body;
- 
- console.log(axios.post(`"https://maps.googleapis.com/maps/api/geocode/json?address="${address}"berlin&key=AIzaSyDYLwxbUeRyQSlAjR9qLXh3pnr4TFCAIW0"`))
-  console.log("req.body.address" )
-  res.render("./search/address");
+  const { address } = req.body;
+
+  axios
+    .get(
+      `"https://maps.googleapis.com/maps/api/geocode/json?address="${address}"berlin&key=AIzaSyDYLwxbUeRyQSlAjR9qLXh3pnr4TFCAIW0"`
+    )
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
+  // console.log("req.body.address" )
+  // res.render("./search/address");
 });
 
 module.exports = router;
