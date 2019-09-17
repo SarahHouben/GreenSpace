ce93f2f8e8de1816263526b3e212df7aaad753fa
Marko <makaveli13@Markos-MacBook-Pro.local>
2019-09-17 16:36:15 +0200

    rendered greenspaces markers,got location from address

--- routes/search.js
+++ routes/search.js
@@ -8,43 +8,37 @@ router.get("/", (req, res, next) => {
   res.render("./search/result-map");
 });
 
-router.post("/", (req, res, next) => {
-  const { lat, lng } = req.body;
-
-  res.render("./search/result-map", {
-    lat,
-    lng,
-    array: JSON.stringify([
-      {
-        lat: 10,
-        lng: 20
-      },
-      {
-        lat: 20,
-        lng: 20
-      },
-      {
-        lat: 30,
-        lng: 20
-      }
-    ])
-  });
-});
 router.get("/location", (req, res, next) => {
-  res.render("./search/location");
+  GreenSpace.find({}, { location: 1, _id: 0 })
+    .then(locations => {
+      let latLongArray = locations.map(obj => {
+        return obj.location;
+      });
+
+      res.render("./search/location", { array: JSON.stringify(latLongArray) });
+    })
+    .catch(err => console.log(err));
 });
 
 router.post("/address", (req, res, next) => {
   const { address } = req.body;
-
   axios
     .get(
-      `"https://maps.googleapis.com/maps/api/geocode/json?address="${address}"berlin&key=AIzaSyDYLwxbUeRyQSlAjR9qLXh3pnr4TFCAIW0"`
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}berlin&key=AIzaSyDYLwxbUeRyQSlAjR9qLXh3pnr4TFCAIW0`
     )
-    .then(res => console.log(res.data))
+    .then(loc => {
+      GreenSpace.find({}, { location: 1, _id: 0 }).then(locations => {
+        let latLongArray = locations.map(obj => {
+          return obj.location;
+        });
+        let data = loc.data.results[0].geometry.location;
+        res.render("./search/address", {
+          data,
+          array: JSON.stringify(latLongArray)
+        });
+      });
+    })
     .catch(err => console.log(err));
-  // console.log("req.body.address" )
-  // res.render("./search/address");
 });
 
 module.exports = router;
